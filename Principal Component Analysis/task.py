5d282eb57afa0f039f52814ef54f2200888eda22
moontacer <montasserthegangsta@gmail.com>
2022-07-20 22:37:19 +0100

    commit

---
+++ Principal Component Analysis/task.py
@@ -0,0 +1,115 @@
+df["Size"] = 0.707 * X["Height"] + 0.707 * X["Diameter"]
+df["Shape"] = 0.707 * X["Height"] - 0.707 * X["Diameter"]
+
+
+##########################################################################################
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from IPython.display import display
+from sklearn.feature_selection import mutual_info_regression
+
+
+plt.style.use("seaborn-whitegrid")
+plt.rc("figure", autolayout=True)
+plt.rc(
+    "axes",
+    labelweight="bold",
+    labelsize="large",
+    titleweight="bold",
+    titlesize=14,
+    titlepad=10,
+)
+
+
+def plot_variance(pca, width=8, dpi=100):
+    # Create figure
+    fig, axs = plt.subplots(1, 2)
+    n = pca.n_components_
+    grid = np.arange(1, n + 1)
+    # Explained variance
+    evr = pca.explained_variance_ratio_
+    axs[0].bar(grid, evr)
+    axs[0].set(
+        xlabel="Component", title="% Explained Variance", ylim=(0.0, 1.0)
+    )
+    # Cumulative Variance
+    cv = np.cumsum(evr)
+    axs[1].plot(np.r_[0, grid], np.r_[0, cv], "o-")
+    axs[1].set(
+        xlabel="Component", title="% Cumulative Variance", ylim=(0.0, 1.0)
+    )
+    # Set up figure
+    fig.set(figwidth=8, dpi=100)
+    return axs
+
+def make_mi_scores(X, y, discrete_features):
+    mi_scores = mutual_info_regression(X, y, discrete_features=discrete_features)
+    mi_scores = pd.Series(mi_scores, name="MI Scores", index=X.columns)
+    mi_scores = mi_scores.sort_values(ascending=False)
+    return mi_scores
+
+
+df = pd.read_csv("../input/fe-course-data/autos.csv")
+
+
+
+
+##########################################################################################
+
+
+
+
+features = ["highway_mpg", "engine_size", "horsepower", "curb_weight"]
+
+X = df.copy()
+y = X.pop('price')
+X = X.loc[:, features]
+
+# Standardize
+X_scaled = (X - X.mean(axis=0)) / X.std(axis=0)
+
+
+
+from sklearn.decomposition import PCA
+
+# Create principal components
+pca = PCA()
+X_pca = pca.fit_transform(X_scaled)
+
+# Convert to dataframe
+component_names = [f"PC{i+1}" for i in range(X_pca.shape[1])]
+X_pca = pd.DataFrame(X_pca, columns=component_names)
+
+X_pca.head()
+
+
+
+loadings = pd.DataFrame(
+    pca.components_.T,  # transpose the matrix of loadings
+    columns=component_names,  # so the columns are the principal components
+    index=X.columns,  # and the rows are the original features
+)
+loadings
+
+
+
+# Look at explained variance
+plot_variance(pca);
+
+
+mi_scores = make_mi_scores(X_pca, y, discrete_features=False)
+mi_scores
+
+
+# Show dataframe sorted by PC3
+idx = X_pca["PC3"].sort_values(ascending=False).index
+cols = ["make", "body_style", "horsepower", "curb_weight"]
+df.loc[idx, cols]
+
+
+df["sports_or_wagon"] = X.curb_weight / X.horsepower
+sns.regplot(x="sports_or_wagon", y='price', data=df, order=2);
